dfa75945a97d03496bca9d21b019f1da32d05e29
reshmyunni <reshmyunni1778@gmail.com>
2022-11-30 17:18:55 +0530

    Initial Commit

---
+++ Sources/MyLibrary/ImageViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ImageViewController.swift
+//  Sample
+//
+//  Created by Reshmy Manuraj on 30/11/22.
+//
+
+import UIKit
+
+public extension UIViewController{
+       
+       public static func getStoryboardVC() -> UIViewController {
+           let storyboard = UIStoryboard(name: String(describing: self), bundle: Bundle.module) // Use Bundle.module
+           return storyboard.instantiateInitialViewController()!
+       }
+}
+public class ImageViewController: UIViewController {
+
+    public static let storyboardVC = UIStoryboard(name: "ImageLib", bundle: Bundle.module).instantiateInitialViewController()!
+    public override func viewDidLoad() {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
