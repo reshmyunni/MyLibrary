20430ad251d0149fefbeaf99217c0ef17e8dd456
reshmyunni <reshmyunni1778@gmail.com>
2022-12-02 11:04:10 +0530

    api update

--- Sources/MyLibrary/APIRequest.swift
+++ Sources/MyLibrary/APIRequest.swift
@@ -17,30 +17,54 @@ public class APIRequest:NSObject{
         print("url:",url as Any)
         guard let requestUrl = url else { fatalError() }
         
-        let task = URLSession.shared.dataTask(with: requestUrl) { (data, response, error) in
-            
-            if error != nil {
-                DispatchQueue.main.async{failure("Unable to reach our servers. Please check your network connection and Retry.")}
-                    return
-                }
-         
-                if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                    let data: Data? = dataString.data(using: .utf8)
-                    if let json = (try? JSONSerialization.jsonObject(with: data!, options: [])) as? [String:Any]
-                    {
+//        let task = URLSession.shared.dataTask(with: requestUrl) { (data, response, error) in
+//
+//            if error != nil {
+//                DispatchQueue.main.async{failure("Unable to reach our servers. Please check your network connection and Retry.")}
+//                    return
+//                }
+//
+//                if let data = data, let dataString = String(data: data, encoding: .utf8) {
+//                    let data: Data? = dataString.data(using: .utf8)
+//                    if let json = (try? JSONSerialization.jsonObject(with: data!, options: [])) as? [String:Any]
+//                    {
+//
+//                            DispatchQueue.main.async{success(json)}
+//                    }
+//                    else
+//                    {
+//                        DispatchQueue.main.async{failure("Unable to reach our servers. Please Retry.")}
+//                    }
+//                }
+//                else
+//                {
+//                    DispatchQueue.main.async{failure("Unable to reach our servers. Please Retry.")}
+//                }
+//        }
+        let task = URLSession.shared.dataTask(with: requestUrl) { data, response, error in
 
-                            DispatchQueue.main.async{success(json)}
-                    }
-                    else
-                    {
-                        DispatchQueue.main.async{failure("Unable to reach our servers. Please Retry.")}
-                    }
-                }
-                else
-                {
-                    DispatchQueue.main.async{failure("Unable to reach our servers. Please Retry.")}
-                }
+            // ensure there is no error for this HTTP response
+            guard error == nil else {
+                print ("error: \(error!)")
+                return
+            }
+            
+            // ensure there is data returned from this HTTP response
+            guard let content = data else {
+                print("No data")
+                return
+            }
+            
+            // serialise the data / NSData object into Dictionary [String : Any]
+            guard let json = (try? JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers)) as? [String: Any] else {
+                print("Not containing JSON")
+                return
+            }
+            
+            print("gotten json response dictionary is \n \(json)")
+            // update UI using the response here
         }
+
         
         task.resume()
     }
